c3b4ecc15e431e223fdcf65e64d4b8cb32e077aa
AmazingBandit <sa.rajbhandari@gmail.com>
2023-12-21 15:00:47 -0800

    created function to populate display, created function to calculate basic math and display result

--- script.js
+++ script.js
@@ -1,30 +1,73 @@
-let num1;
-let operator;
-let num2;
+const add = (num1, num2) => {
+  num1 = Number(num1);
+  num2 = Number(num2);
+  return num1 + num2;
+};
+const subtract = (num1, num2) => {
+  num1 = Number(num1);
+  num2 = Number(num2);
+  return num1 - num2;
+};
 
-//stores final value
-let value;
+const multiply = (num1, num2) => {
+  num1 = Number(num1);
+  num2 = Number(num2);
+  return num1 * num2;
+};
 
+const divide = (num1, num2) => {
+  num1 = Number(num1);
+  num2 = Number(num2);
+  return num1 / num2;
+};
+
+let counter = 0;
+
+const textbox = document.querySelector(".textbox");
 const btn = document.querySelectorAll(".btn");
+const operBtn = document.querySelectorAll(".operBtn");
+const equalBtn = document.querySelector(".equalBtn");
+const clearBtn = document.querySelector(".clearBtn");
 
-//determine what operation to call
+let num1, operator, num2;
+
 const operate = (num1, operator, num2) => {
   switch (operator) {
     case "+":
-      add(num1, num2);
+      return (textbox.value = add(num1, num2));
     case "-":
-      subtract(num1, num2);
+      return (textbox.value = subtract(num1, num2));
     case "*":
-      multiply(num1, num2);
+      return (textbox.value = multiply(num1, num2));
     case "/":
-      divide(num1, num2);
+      return (textbox.value = divide(num1, num2));
   }
 };
 
-const add = (num1, num2) => {};
+btn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    textbox.value += btn.textContent;
+  });
+});
 
-const subtract = (num1, num2) => {};
+operBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    textbox.value += ` ${btn.textContent} `;
+  });
+});
 
-const multiply = (num1, num2) => {};
+equalBtn.addEventListener("click", () => {
+  let display = textbox.value;
+  const array = display.split(" ");
+  num1 = array[0];
+  operator = array[1];
+  num2 = array[2];
+  operate(num1, operator, num2);
+});
 
-const divide = (num1, num2) => {};
+clearBtn.addEventListener("click", () => {
+  textbox.value = "";
+  num1 = 0;
+  num2 = 0;
+  operator = "";
+});
